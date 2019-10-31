1e1576dd9fd3e0c6075c027cad4891518af9fc46
Lenny <55984010+LennyMueller@users.noreply.github.com>
2019-10-31 14:58:58 +0100

    Roadmap changes

--- Roadmap/src/script/lib.js
+++ Roadmap/src/script/lib.js
@@ -67,16 +67,6 @@ function startAnimatedGradient() {
   animatedGradient = setTimeout(startAnimatedGradient, timer);
 }
 
-/*** PAGE INTERAKTIVITY ***/
-function openRoadmap(id) {
-  /* 
-   * open roadmap suppage and adds the SemesterID as parameter in URL
-   *
-   * @parameter {String}: ID of Semester
-   */
-  window.open("roadmap.html?id=" + id);
-}
-
 /*** Dropdown-Menü ***/
 function myFunction() {
   /*
--- Roadmap/src/script/roadmap.js
+++ Roadmap/src/script/roadmap.js
@@ -3,3 +3,21 @@
   var semesterNumber = id.charAt(id.length-1);
   document.getElementById("headline").innerHTML = "Semester <span>" + semesterNumber + "</span>";
 }
+
+function edit(id) {
+  id; 
+  //lelelele
+}
+
+function safeChanges() {
+
+}
+
+function deleteEvent() {
+
+}
+
+function addEvent() {
+  document.getElementById("addButton").style.display = "none";
+  document.getElementById("addEvent").style.display = "visable";
+}
