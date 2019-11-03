3c3ee007cf8ebc68a30309bfd389fdb9f1b0dec5
ksrssg <54836233+ksrssg@users.noreply.github.com>
2019-11-03 12:05:30 +0100

    cursor change, responsive

--- Roadmap/src/script/home.js
+++ Roadmap/src/script/home.js
@@ -1,8 +1,77 @@
-function openRoadmap(id) {
-    /* 
-     * open roadmap suppage and adds the SemesterID as parameter in URL
-     *
-     * @parameter {String}: ID of Semester
-     */
-    window.open("roadmap.html?id=" + id);
+function openRoadmap(id) {
+        /* 
+         * open roadmap suppage and adds the SemesterID and ListID as parameters in URL
+         *
+         * @parameter {String}: ID of Semester
+         * @return {}
+         */
+        //window.open("roadmap.html?semester=" + id + "&listid=" + listIDs[id.charAt(id.length-1)]);
+        window.open("roadmap.html?id=" + id);
+    } 
+
+    window.onload = () => {
+            /*
+             * update listIDs
+             */
+            getAllLists(false);
+        } 
+
+      
+    function changesemestertext(id) {
+
+        document.getElementById("speechbubble").innerHTML = "Semester " + id.charAt(id.length-1);
+    }
+
+    function changetextback() {
+        document.getElementById("speechbubble").innerHTML = "Wähle ein Semester aus!"
+    }
+
+   var opacity = 0;
+   var intervalId = 0;
+
+   function fadeOut() {
+       intervalId=setInterval(hide,20);
+   }
+
+   function fadeIn() {
+       intervalId=setInterval(show,20);
+   }
+
+   function showSb() {
+    var img = document.getElementById("speechbubble");
+    opacity = Number(window.getComputedStyle(img).getPropertyValue("opacity"));
+    if(opacity<1){
+        opacity=opacity+0.1;
+        img.style.opacity=opacity;
+    }
+    else{
+        clearInterval(intervalId);
+    }
 }
+
+   function hideSb() {
+       var img = document.getElementById("speechbubble");
+       opacity = Number(window.getComputedStyle(img).getPropertyValue("opacity"));
+       if(opacity>0){
+           opacity=opacity-0.1;
+           img.style.opacity=opacity;
+       }
+       else{
+           clearInterval(intervalId);
+       }
+   }
+
+
+ //Auto-play von spaceaudio nach einer Sekunde 
+ /*window.setInterval(function() {
+    var myaudio = document.getElementById("playAudio").play();
+  }, 1000);
+    */
+  window.setInterval(function() {
+
+  
+  var textArray = ["Mein Name ist Ali!", "Wähle ein Semester aus!", "Halloooo! Na, du?", "Jetzt drück endlich was...", "Nur noch ... Tage bis zu dein Studium zu Ende ist!"];
+  var randomIndex = Math.floor(Math.random() * textArray.length); 
+  var randomElement = textArray[randomIndex];
+  document.getElementById("speechbubble").innerHTML = randomElement;
+  }, 5000);
